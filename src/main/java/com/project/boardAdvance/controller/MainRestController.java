901e43907dab38e781ead558169f820fe8289d4c
alicesykim95 <alicesykim95@gmail.com>
2022-03-11 23:50:06 +0900

    edit

--- src/main/java/com/project/boardAdvance/controller/MainRestController.java
+++ src/main/java/com/project/boardAdvance/controller/MainRestController.java
@@ -1,4 +1,7 @@
 package com.project.boardAdvance.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainRestController {
 }
--- src/main/java/com/project/boardAdvance/repository/UserRepository.java
+++ src/main/java/com/project/boardAdvance/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.project.boardAdvance.repository;
 
 import com.project.boardAdvance.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
